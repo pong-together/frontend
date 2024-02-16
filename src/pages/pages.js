9082864d87919b65355877d8efec4f8a7730a2a8
jonseo <jonseo@student.42seoul.kr>
2024-02-16 21:53:38 +0900

    feat/pong-together/frontend#17: Remote 기본 레이아웃 완료 및 클러스터 맥 git 연동

---
+++ src/pages/pages.js
@@ -0,0 +1,25 @@
+import Login from './components/Login.js';
+import GameSelect from './components/game_select/GameSelect.js';
+
+import Tournament from './components/Tournament.js';
+import Remote from './components/Remote/Remote.js';
+
+import Local from './components/Local.js';
+
+export default (main) => {
+	const login = () => new Login(main);
+	const gameSelect = () => new GameSelect(main);
+	const local = () => new Local(main);
+	const tournament = () => new Tournament(main);
+	const remote = () => new Remote(main);
+	const game = () => null;
+
+	return {
+		login,
+		gameSelect,
+		local,
+		tournament,
+		remote,
+		game,
+	};
+};
